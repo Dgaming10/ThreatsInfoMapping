0952c60ba6454785e1b17646433ab08bede52bd8
Dgaming10 <85754917+Dgaming10@users.noreply.github.com>
2024-04-24 20:42:46 +0300

    Update main.py
    added description, changed the color of the markers

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def get_res(map_url_req, c_name) -> tuple:
     res_i = {}
     try:
         res_i = requests.get(map_url_req).json()['results']
+        print(res_i)
         if 'name' not in res_i[0].keys():
             in_name = c_name
         else:
@@ -23,12 +24,14 @@ def get_res(map_url_req, c_name) -> tuple:
     return in_name, res_i[0]['lat'], res_i[0]['lon']
 
 
-list_places = set([i['data'].strip() for i in requests.get(sirens_url).json()])
-print(list_places)
+sirens_res = requests.get(sirens_url).json()
+list_tuple_places = set([(i['data'].strip(), i['category_desc']) for i in sirens_res])
+list_places = list(set([i['data'].strip() for i in sirens_res]))
+print(list_tuple_places)
 m = folium.Map(location=[32.0879048, 34.7148434], zoom_start=7)  # LOCATION OF TEL AVIV
 len_list = len(list_places)
 i = 0
-for c in list(list_places):
+for c in list_places:
     print(f'currently at "{c}" \t ,{len_list - i} remaining')
     i += 1
     map_url = f"https://api.geoapify.com/v1/geocode/search?text={c}&format=json&apiKey={API_KEYS[0]}"
@@ -37,9 +40,9 @@ for c in list(list_places):
         if len(ans_tup) > 0:
             folium.Marker(
                 location=[ans_tup[1], ans_tup[2]],
-                tooltip="Click me!",
-                popup=ans_tup[0],
-                icon=folium.Icon(icon="cloud"),
+                tooltip="Threat!",
+                popup=f"<b>{ans_tup[0]}</b><br>" + ','.join([i[1].strip() for i in list_tuple_places if i[0] == c]),
+                icon=folium.Icon(icon="info-sign", color='red')
             ).add_to(m)
     except requests.exceptions.ConnectionError as p:
         print("ERROR", p, "\nTRYING TO RESTORE")
@@ -50,9 +53,9 @@ for c in list(list_places):
                 print("SUCCEEDED!", API_KEYS[0])
                 folium.Marker(
                     location=[ans_tup_er[1], ans_tup_er[2]],
-                    tooltip="Click me!",
-                    popup=ans_tup_er[0],
-                    icon=folium.Icon(icon="cloud"),
+                    tooltip="Threat",
+                    popup=f"<b>{ans_tup_er[0]}</b><br>" + ','.join([i[1].strip() for i in list_tuple_places if i[0] == c]),
+                    icon=folium.Icon(icon="info-sign", color='red')
                 ).add_to(m)
                 break
             API_KEYS.pop(0)
